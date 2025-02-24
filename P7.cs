cbc7dcbae19d9a00142ca15635aabe4177214cbe
Sunnycfr099 <sunnytanwar242412@gmail.com>
2025-02-24 10:12:03 +0530

    Create P7.cs

---
+++ P7.cs
@@ -0,0 +1,27 @@
+using System;
+
+class PassByDemo
+{
+    static void PassByValue(int x)
+    {
+        x += 10;
+    }
+
+    static void PassByReference(ref int x)
+    {
+        x += 10;
+        
+    }
+    static void Main()
+    {
+        int num = 10;
+        
+         PassByValue(num);
+         Console.WriteLine($"After PassByValue: {num}");
+         
+         PassByReference(ref num);
+         Console.WriteLine($"After PassByReference: {num}");
+         
+    }
+    
+}
